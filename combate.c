d0bee6884c1f1b63c76298604dde54656b3d10a3
P-Ancelmo <51537343+P-Ancelmo@users.noreply.github.com>
2019-12-02 21:15:02 -0300

    Update combate.c

--- combate.c
+++ combate.c
@@ -607,8 +607,8 @@ void combate(INIMIGO inimigo)
 
     }
 
-    if(grupo[i].xp >=  grupo[i].xpmax)
-      printf("\nParabens, o grupo subiu de level \n");
+    /*if(grupo[i].xp >=  grupo[i].xpmax)
+      printf("\nParabens, o grupo subiu de level \n");*/
 
     if(inimigo.hp <= 0)
     {
